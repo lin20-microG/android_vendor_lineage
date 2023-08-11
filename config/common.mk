834048234d310729894192a7bdc6ae6c622341eb
MSe <mse1969@posteo.de>
2023-08-11 16:40:29 +0200

    config: Remove Lineage extra keys
    Change-Id: Ieafc9299c96b92a68cdcffae2120864854c19adb

--- config/common.mk
+++ config/common.mk
@@ -249,8 +249,6 @@ CUSTOM_LOCALES += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
 
-PRODUCT_EXTRA_RECOVERY_KEYS += \
-    vendor/lineage/build/target/product/security/lineage
 
 include vendor/lineage/config/version.mk
 
