93dfcb2778d1c6538b2cc6afe00b5d72a84558b4
Aaron Kling <webgeek1234@gmail.com>
2024-03-07 11:56:57 -0600

    config: Build simple Settings provisioning service
    Similar to SimpleDeviceConfig, this allows devices to initialize options
    in the normal settings storage.
    
    Change-Id: I58ae47dc21b0d85ac1ec01afda187820919fef7f

--- config/common.mk
+++ config/common.mk
@@ -152,7 +152,8 @@ PRODUCT_COPY_FILES += \
 
 # Config
 PRODUCT_PACKAGES += \
-    SimpleDeviceConfig
+    SimpleDeviceConfig \
+    SimpleSettingsConfig
 
 # Extra tools in Lineage
 PRODUCT_PACKAGES += \
